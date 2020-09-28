74566fe8b77ea69d4d35c8f1812df8c0a5c5da60
T.Tian <lovaulonzezoeta@gmail.com>
2020-09-28 11:24:29 +0200

    try with multiple window setup

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 from pathlib import Path
 
 
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow
 from PyQt5.QtCore import QFile, Qt
 from PyQt5 import uic
 
@@ -16,6 +16,36 @@ class MainWindow(QWidget):
     def load_ui(self):
         curpath = Path(__file__).parent
         path = curpath / "ui" / "control_panel.ui"
+        ui_file = QFile(path.as_posix())
+        ui_file.open(QFile.ReadOnly)
+        uic.loadUi(ui_file, self)
+        ui_file.close()
+        self.button_side_cam.clicked.connect(self.fun_window_side_cam)
+        self.button_top_cam.clicked.connect(self.fun_window_top_cam)
+
+    def fun_window_side_cam(self):
+        # Start a side cam window
+        if not hasattr(self, "window_side_cam"):
+            self.window_side_cam = VideoWindow(title="Side Camera")
+        self.window_side_cam.show()
+
+    def fun_window_top_cam(self):
+        # Start a side cam window
+        if not hasattr(self, "window_top_cam"):
+            self.window_top_cam = VideoWindow(title="Top Camera")
+        self.window_top_cam.show()
+
+            
+class VideoWindow(QWidget):
+    def __init__(self, title=""):
+        super(QWidget, self).__init__()
+        self.load_ui()
+        if title != "":
+            self.setWindowTitle(title)
+
+    def load_ui(self):
+        curpath = Path(__file__).parent
+        path = curpath / "ui" / "video_window.ui"
         ui_file = QFile(path.as_posix())
         ui_file.open(QFile.ReadOnly)
         uic.loadUi(ui_file, self)
