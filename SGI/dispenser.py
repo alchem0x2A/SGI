b5107bd39f1606fdd2779fe4d6e8f74fbeaf022a
Special Admin <spadmin@ICB-SHIH-21.d.ethz.ch>
2020-10-16 15:54:58 +0200

    Add functionality for controlling serial port

---
+++ SGI/dispenser.py
@@ -0,0 +1,35 @@
+"""Provide a simple method to connect to 
+the dispenser via serial port
+"""
+import serial
+import time
+
+# Initialization required!
+
+def make_droplet(port="COM3", baud=9600, timeout=0.5, parity="even"):
+    if parity == "even":
+        par = serial.PARITY_EVEN
+    elif parity == "odd":
+        par = serial.PARITY_ODD
+    elif parity == "":
+        par = serial.PARITY_NONE
+
+    with serial.Serial(port, baud, timeout=timeout, parity=par) as ser:
+        if not ser.is_open:
+            result = False
+        else:
+            ser.write(b"ST 7100\r\n")
+            b = ser.readline()
+            print("Read", b)
+            if "OK" in b.decode("ascii"):
+                time.sleep(0.2)
+                result = True
+            else:
+                result = False
+    return result
+    
+def main():
+    print(make_droplet())
+
+if __name__ == "__main__":
+    main()
